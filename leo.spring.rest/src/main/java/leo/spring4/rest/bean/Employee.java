792ef2217cec1121ecd4e2a4fba16fe13b191a17
Vito Lee <plummoon@gmail.com>
2016-10-25 18:17:24 +0800

    新增代码文件

--- leo.spring.rest/src/main/java/leo/spring4/rest/bean/Employee.java
+++ leo.spring.rest/src/main/java/leo/spring4/rest/bean/Employee.java
@@ -1,7 +1,40 @@
 package leo.spring4.rest.bean;
+import javax.xml.bind.annotation.XmlRootElement;
 
-/**
- * Created by plumm on 2016-10-25.
- */
+@XmlRootElement(name="employee")
 public class Employee {
+
+    private long id;
+    private String name;
+    private String email;
+
+    public Employee() {}
+
+    public Employee(long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
 }
--- leo.spring.rest/src/main/java/leo/spring4/rest/controller/EmployeeController.java
+++ leo.spring.rest/src/main/java/leo/spring4/rest/controller/EmployeeController.java
@@ -1,43 +1,46 @@
 package leo.spring4.rest.controller;
 
-/**
- * Created by plumm on 2016-10-10.
- */
+import java.io.StringReader;
+import java.util.List;
+
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
 
 import leo.spring4.rest.bean.Employee;
 import leo.spring4.rest.bean.EmployeeList;
+import leo.spring4.rest.ds.EmployeeDS;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.View;
-
-import javax.xml.transform.Source;
-import javax.xml.transform.stream.StreamSource;
-import java.io.StringReader;
-import java.util.List;
 
 @Controller
 public class EmployeeController {
+
+    private EmployeeDS employeeDS;
+
+    public void setEmployeeDS(EmployeeDS ds) {
+        this.employeeDS = ds;
+    }
+
+    private Jaxb2Marshaller jaxb2Mashaller;
+
+    public void setJaxb2Mashaller(Jaxb2Marshaller jaxb2Mashaller) {
+        this.jaxb2Mashaller = jaxb2Mashaller;
+    }
+
     private static final String XML_VIEW_NAME = "employees";
 
-    @RequestMapping(method = RequestMethod.GET, value = "/employee/{id}")
+    @RequestMapping(method=RequestMethod.GET, value="/employee/{id}")
     public ModelAndView getEmployee(@PathVariable String id) {
         Employee e = employeeDS.get(Long.parseLong(id));
         return new ModelAndView(XML_VIEW_NAME, "object", e);
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "/employee")
-    public ModelAndView addEmployee(@RequestBody String body) {
-        Source source = new StreamSource(new StringReader(body));
-        Employee e = (Employee) jaxb2Mashaller.unmarshal(source);
-        employeeDS.add(e);
-        return new ModelAndView(XML_VIEW_NAME, "object", e);
-    }
-
-    @RequestMapping(method = RequestMethod.PUT, value = "/employee/{id}")
+    @RequestMapping(method=RequestMethod.PUT, value="/employee/{id}")
     public ModelAndView updateEmployee(@RequestBody String body) {
         Source source = new StreamSource(new StringReader(body));
         Employee e = (Employee) jaxb2Mashaller.unmarshal(source);
@@ -45,11 +48,27 @@ public class EmployeeController {
         return new ModelAndView(XML_VIEW_NAME, "object", e);
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "/employee/{id}")
+    @RequestMapping(method=RequestMethod.POST, value="/employee")
+    public ModelAndView addEmployee(@RequestBody String body) {
+        Source source = new StreamSource(new StringReader(body));
+        Employee e = (Employee) jaxb2Mashaller.unmarshal(source);
+        employeeDS.add(e);
+        return new ModelAndView(XML_VIEW_NAME, "object", e);
+    }
+
+    @RequestMapping(method=RequestMethod.DELETE, value="/employee/{id}")
     public ModelAndView removeEmployee(@PathVariable String id) {
         employeeDS.remove(Long.parseLong(id));
         List<Employee> employees = employeeDS.getAll();
         EmployeeList list = new EmployeeList(employees);
         return new ModelAndView(XML_VIEW_NAME, "employees", list);
     }
+
+    @RequestMapping(method=RequestMethod.GET, value="/employees")
+    public ModelAndView getEmployees() {
+        List<Employee> employees = employeeDS.getAll();
+        EmployeeList list = new EmployeeList(employees);
+        return new ModelAndView(XML_VIEW_NAME, "employees", list);
+    }
+
 }
