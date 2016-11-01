4ec5168dee49772cbaac8ba85de22a98cb80b10b
Vito Lee <plummoon@gmail.com>
2016-11-01 21:31:18 +0800

    新增代码文件

--- leo.spring.rest/src/main/java/leo/spring4/rest/service/EmployeeService.java
+++ leo.spring.rest/src/main/java/leo/spring4/rest/service/EmployeeService.java
@@ -3,13 +3,14 @@ package leo.spring4.rest.service;
 import leo.spring4.rest.bean.EmployeeList;
 
 /**
- * Created by plummoon on 16/10/30.
+ * Created by YueLi on 16/10/30.
+ *
  */
 public interface EmployeeService {
 
     /**
      * 获取雇员信息
-     * @return
+     * @return 雇员列表
      */
     EmployeeList GetEmployee();
 }
